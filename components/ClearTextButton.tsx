eea5db62767cd1e55991082fd85e9213dfd51ab2
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-20 07:16:53 +0800

    Add ClearTextButton component

---
+++ components/ClearTextButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ClearTextButton() {
+  return (
+    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl rounded-xl bg-red-500">
+      Clear Text
+    </button>
+  );
+}
