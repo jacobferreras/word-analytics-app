03b54ecc769216bea372e65483e7b5d5bcc510d7
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-20 07:16:43 +0800

    Update Home component layout with background color and add TextAreaContainer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Header from "@/components/Header";
+import TextAreaContainer from "@/components/TextAreaContainer";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-white min-h-screen">
+      <div className="bg-[#e2e8f0] min-h-screen">
         <Header />
+        <div className="flex justify-center py-12">
+          <TextAreaContainer />
+        </div>
       </div>
     </>
   );
