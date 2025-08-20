0e2a728719426ab6087ff387d1fbf48cdb0a5c87
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-20 08:57:14 +0800

    Add AnalyticsContainer component for displaying analysis results and platform limits

---
+++ components/AnalyticsContainer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Count from "./Count";
+import PlatformCount from "./PlatfromCount";
+
+export default function AnalyticsContainer() {
+  return (
+    <div className="card w-96 bg-white card-xl shadow-sm">
+      <div className="card-body">
+        <h2 className=" text-black font-bold text-center text-2xl mb-2">
+          Analysis Result
+        </h2>
+        <div className="gap-y-4">
+          <Count label="Word Count" count={0} />
+          <Count label="Character Count" count={0} />
+          <Count label="Sentence Count" count={0} />
+          <Count label="Paragraph Count" count={0} />
+        </div>
+
+        <h2 className=" text-gray-700 font-bold text-start text-2xl mt-2">
+          Platform Limits
+        </h2>
+        <div>
+          <PlatformCount label="Facebook" count={63206} />
+          <PlatformCount label="Instagram" count={2200} />
+        </div>
+      </div>
+    </div>
+  );
+}
