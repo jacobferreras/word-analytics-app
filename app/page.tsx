3cea6c4b2af4ea912f643d692eddc66a5dd20002
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-20 08:34:27 +0800

    Update Home component to include Footer and AnalyticsContainer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import AnalyticsContainer from "@/components/AnalyticsContainer";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import TextAreaContainer from "@/components/TextAreaContainer";
 
@@ -6,10 +8,12 @@ export default function Home() {
     <>
       <div className="bg-[#e2e8f0] min-h-screen">
         <Header />
-        <div className="flex justify-center py-12">
+        <div className="flex justify-center py-12 gap-4">
           <TextAreaContainer />
+          <AnalyticsContainer />
         </div>
       </div>
+      <Footer />
     </>
   );
 }
