01da58822df01343ac19ee9e329c0680fde79044
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-20 15:33:59 +0800

    Remove AnalyticsContainer and TextAreaContainer from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
-import AnalyticsContainer from "@/components/AnalyticsContainer";
+import Container from "@/components/Container";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import TextAreaContainer from "@/components/TextAreaContainer";
 
 export default function Home() {
   return (
     <>
       <div className="bg-[#e2e8f0] min-h-screen">
         <Header />
-        <div className="flex justify-center py-12 gap-4">
-          <TextAreaContainer />
-          <AnalyticsContainer />
-        </div>
+        <Container />
       </div>
       <Footer />
     </>
