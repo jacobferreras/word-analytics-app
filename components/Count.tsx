9cb667725e7b50e135d8b9af9a49d1cf75ce2645
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-21 11:02:00 +0800

    Refactor Count and PlatformCount components for improved layout and readability

--- components/Count.tsx
+++ components/Count.tsx
@@ -7,8 +7,9 @@ interface CountProps {
 
 export default function Count({ count, label }: CountProps) {
   return (
-    <p className="text-gray-700 text-xl font-semibold">
-      {label}: <span className="text-blue-700">{count}</span>
-    </p>
+    <div>
+      <h2 className="text-blue-700 text-5xl font-bold text-center">{count}</h2>
+      <p className="text-gray-700 text-xl font-bold text-center">{label}</p>
+    </div>
   );
 }
--- components/PlatfromCount.tsx
+++ components/PlatfromCount.tsx
@@ -4,16 +4,26 @@ interface PlatformCountProps {
   label: string;
   count: number;
   limit: string;
+  icon: string;
 }
 
 export default function PlatformCount({
   label,
   count,
   limit,
+  icon,
 }: PlatformCountProps) {
   return (
-    <p className="text-gray-700 text-xl font-semibold">
-      {label}({limit} Chars): <span className="text-blue-700">{count}</span>
-    </p>
+    <div>
+      <h2 className="text-blue-700 font-semibold text-center text-5xl mb-4">
+        {count}
+      </h2>
+      <h2 className="text-3xl text-black text-center">
+        <i className={`${icon}`}></i>
+      </h2>
+      <p className="text-gray-700 text-xl font-bold text-center">
+        {label}({limit} Chars)
+      </p>
+    </div>
   );
 }
