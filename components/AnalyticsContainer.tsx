6f907f802defb08333009805bf35115ccae985b4
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-08-21 11:01:52 +0800

    Refactor AnalyticsContainer layout for improved responsiveness and alignment

--- components/AnalyticsContainer.tsx
+++ components/AnalyticsContainer.tsx
@@ -21,24 +21,34 @@ const AnalyticsContainer = ({
   instagram,
 }: AnalyticsContainerProps) => {
   return (
-    <div className="card w-auto md:w-96 bg-white card-xl shadow-sm">
+    <div className="card w-auto md:w-124 bg-white card-xl shadow-sm">
       <div className="card-body">
-        <h2 className=" text-black font-bold text-start text-3xl mb-2">
+        <h2 className=" text-black font-bold text-center text-3xl mb-2">
           Analysis Result
         </h2>
-        <div className="gap-y-4">
+        <div className="grid grid-cols-2 gap-8">
           <Count label="Word Count" count={word} />
           <Count label="Character Count" count={character} />
           <Count label="Sentence Count" count={sentence} />
           <Count label="Paragraph Count" count={paragraph} />
         </div>
 
-        <h2 className=" text-gray-700 font-bold text-start text-2xl mt-2">
+        <h2 className=" text-gray-700 font-bold text-center text-2xl my-4">
           Platform Limits
         </h2>
-        <div>
-          <PlatformCount label="Facebook" count={facebook} limit="63,206" />
-          <PlatformCount label="Instagram" count={instagram} limit="2,200" />
+        <div className="flex flex-row">
+          <PlatformCount
+            label="Facebook"
+            count={facebook}
+            limit="63,206"
+            icon="bi bi-facebook"
+          />
+          <PlatformCount
+            label="Instagram"
+            count={instagram}
+            limit="2,200"
+            icon="bi bi-instagram"
+          />
         </div>
       </div>
     </div>
